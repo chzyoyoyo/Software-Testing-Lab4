cccb39215b528d5fe936d4b10dbc67d8f2762ae2
chzyoyoyo <63632978+chzyoyoyo@users.noreply.github.com>
2020-04-15 11:39:52 +0800

    Update PriorityQueueTest.java

--- src/test/java/PriorityQueueTest.java
+++ src/test/java/PriorityQueueTest.java
@@ -1,7 +1,6 @@
 
 
 import org.junit.jupiter.api.Test;
-import org.testng.annotations.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 import java.util.PriorityQueue;
